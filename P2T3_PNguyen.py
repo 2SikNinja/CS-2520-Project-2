b648d0bf63d722b025cfb85c27ae5f88b44c72e3
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-09-28 03:07:13 -0700

    completed task 3

--- P2T3_PNguyen.py
+++ P2T3_PNguyen.py
@@ -2,4 +2,25 @@
 #Assignment: Lab #4
 #Completed (or last revision): 10/11/2022
 
-import turtle
+import digitprint
+
+def zipCodeReader(zipCode):
+    userInput = zipCode
+    userInput = userInput.replace('-',"")
+    return userInput
+
+def checkSum(zipCode):
+    zipCodeList = list(zipCode)
+    placeHolder = 0
+    for i in zipCodeList:
+        placeHolder += int(i)
+    checkSum = 10-(placeHolder%10)
+    return str(checkSum)
+
+
+def main():
+    userInput = input("Please enter a zip code: ")
+    checkSum(zipCodeReader(userInput))
+    digitprint.digitPrint(zipCodeReader(userInput)+checkSum(zipCodeReader(userInput)))
+    
+main()
---
+++ digitprint.py
@@ -0,0 +1,112 @@
+#Authors: Peter Nguyen
+#Assignment: Lab #4
+#Completed (or last revision): 10/11/2022
+
+def digitPrint(zip) :
+        printStartStop()
+        for i in zip:
+                if i == '0':
+                        printZero()
+                elif i == '1':
+                        printOne()
+                elif i == '2':
+                        printTwo()
+                elif i == '3':
+                        printThree()
+                elif i == '4':
+                        printFour()
+                elif i == '5':
+                        printFive()
+                elif i == '6':
+                        printSix()
+                elif i == '7':
+                        printSeven()
+                elif i == '8':
+                        printEight()
+                else:
+                        printNine()
+        printStartStop()
+
+def printZero():
+        zeroBinary = [1, 1, 0, 0, 0]
+        for number in zeroBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+                
+
+def printOne():
+        oneBinary = [0, 0, 0, 1, 1]
+        for number in oneBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printTwo():
+        twoBinary = [0, 0, 1, 0, 1]
+        for number in twoBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printThree():
+        threeBinary = [0, 0, 1, 1, 0]
+        for number in threeBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printFour():
+        fourBinary = [0, 1, 0, 0, 1]
+        for number in fourBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printFive():
+        fiveBinary = [0, 1, 0, 1, 0]
+        for number in fiveBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printSix():
+        sixBinary = [0, 1, 1, 0, 0]
+        for number in sixBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printSeven():
+        sevenBinary = [1, 0, 0, 0, 1]
+        for number in sevenBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printEight():
+        eightBinary = [1, 0, 0, 1, 0]
+        for number in eightBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printNine():
+        nineBinary = [1, 0, 1, 0, 0]
+        for number in nineBinary:
+                if number == 1:
+                        print('┃', end='')
+                else:
+                        print('╻', end='')
+
+def printStartStop():
+        print('┃', end = '')
