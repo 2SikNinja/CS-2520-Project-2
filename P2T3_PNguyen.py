7a70b7269b37b9e452197830228b38e819de827e
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-02 18:42:30 -0700

    added outputs

--- P2T3_PNguyen.py
+++ P2T3_PNguyen.py
@@ -25,3 +25,18 @@ def main():
     digitprint.digitPrint(zipCodeReader(userInput)+checkSum(zipCodeReader(userInput)))
     
 main()
+
+#Outputs
+# Please enter a zip code: 55555-1237
+# The checksum is: 2
+# ┃╻┃╻┃╻╻┃╻┃╻╻┃╻┃╻╻┃╻┃╻╻┃╻┃╻╻╻╻┃┃╻╻┃╻┃╻╻┃┃╻┃╻╻╻┃╻╻┃╻┃┃
+
+# Please enter a zip code: 91007-1000
+# The checksum is: 2
+# ┃┃╻┃╻╻╻╻╻┃┃┃┃╻╻╻┃┃╻╻╻┃╻╻╻┃╻╻╻┃┃┃┃╻╻╻┃┃╻╻╻┃┃╻╻╻╻╻┃╻┃┃
+
+# Please enter a zip code: 91754-0001
+# The checksum is: 3
+# ┃┃╻┃╻╻╻╻╻┃┃┃╻╻╻┃╻┃╻┃╻╻┃╻╻┃┃┃╻╻╻┃┃╻╻╻┃┃╻╻╻╻╻╻┃┃╻╻┃┃╻┃
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-newWords = newWords.replace('\\t',"")
+
+# Please enter a zip code: 91007-1000
+# The checksum is: 2
+# ┃┃╻┃╻╻╻╻╻┃┃┃┃╻╻╻┃┃╻╻╻┃╻╻╻┃╻╻╻┃┃┃┃╻╻╻┃┃╻╻╻┃┃╻╻╻╻╻┃╻┃┃
